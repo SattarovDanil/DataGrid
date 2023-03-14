1bed30dac554af9c3b58f2e9f30e94f75311eae4
SattarovDanil <vimx123456@gmail.com>
2023-03-14 20:25:40 +0400

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Teacher> _data;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            _data = new ObservableCollection<Teacher>();
+
+            dt_grid.ItemsSource = _data;
+
+            Teacher teach = new Teacher()
+            {
+                Name = "Заводский Игор",
+                ContactNumber = "8 800 555 35 35",
+            };
+            _data.Add(teach);
+        }
+
+        private void btn(object sender, RoutedEventArgs e)
+        {
+            var s = (e.OriginalSource as Button).DataContext;
+            Teacher current = (Teacher)s;
+
+            _data.Remove(current);
+            
+            //_data.Remove((Teacher)dt_grid.SelectedItem);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TaskWindow taskWindow = new TaskWindow();
+            taskWindow.Show();
+        }
+    }
+
+    class Teacher
+    {
+        public string Name { get; set; }
+        public string ContactNumber { get; set; }
+    }
+}
